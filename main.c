b44ebcdf7205c22bf5d4c37b2b2214290f114f77
StefaniusSuperbus <stefaniussuperbus@gmail.com>
2019-07-21 18:15:36 +0200

    dodat voz sa šinama

--- main.c
+++ main.c
@@ -226,6 +226,267 @@ void draw_sphere(){
     glutSolidSphere(0.1,32,32);
     glPopMatrix();
 }
+void draw_rails(){
+    /* Koeficijenti ambijentalne refleksije materijala. */
+    GLfloat ambient_coeffs[] = { 0.3, 0.3, 0.3 };
+
+    /* Koeficijenti difuzne refleksije materijala. */
+    GLfloat diffuse_coeffs[] = { 0.4, 0.4, 0.4};
+
+    /* Koeficijenti spekularne refleksije materijala. */
+    GLfloat specular_coeffs[] = { 0.775,0.775,0.775};
+
+    /* Koeficijent glatkosti materijala. */
+    GLfloat shininess = 70;
+
+
+    /* Podesavaju se parametri materijala. */
+    glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_coeffs);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, diffuse_coeffs);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, specular_coeffs);
+    glMaterialf(GL_FRONT, GL_SHININESS, shininess);
+
+    glShadeModel(GL_SMOOTH);
+    glPushMatrix();
+    glScalef(0.005,0.01,100);
+    glutSolidCube(1);
+    glPopMatrix();
+}
+void draw_plank(){
+    /* Koeficijenti ambijentalne refleksije materijala. */
+    GLfloat ambient_coeffs[] = { 0.7, 0.7, 0, 1 };
+
+    /* Koeficijenti difuzne refleksije materijala. */
+    GLfloat diffuse_coeffs[] = { 0.7, 0.7, 0, 1 };
+
+    /* Koeficijenti spekularne refleksije materijala. */
+    GLfloat specular_coeffs[] = { 0, 0, 0.7, 1 };
+
+    /* Koeficijent glatkosti materijala. */
+    GLfloat shininess = 40;
+
+
+    /* Podesavaju se parametri materijala. */
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient_coeffs);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse_coeffs);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular_coeffs);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
+
+    glShadeModel(GL_SMOOTH);
+    glPushMatrix();
+    glScalef(0.2,0.05,0.1);
+    glutSolidCube(1);
+    glPopMatrix();
+}
+void draw_wheel(){
+    /* Koeficijenti ambijentalne refleksije materijala. */
+    GLfloat ambient_coeffs[] = { 1,0,0, 1 };
+
+    /* Koeficijenti difuzne refleksije materijala. */
+    GLfloat diffuse_coeffs[] = { 1,0,0, 1 };
+
+    /* Koeficijenti spekularne refleksije materijala. */
+    GLfloat specular_coeffs[] = { 1,0,0, 1 };
+
+    /* Koeficijent glatkosti materijala. */
+    GLfloat shininess = 20;
+
+
+    /* Podesavaju se parametri materijala. */
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient_coeffs);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse_coeffs);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular_coeffs);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
+    glDisable(GL_CULL_FACE);
+    glShadeModel(GL_SMOOTH);
+    glPushMatrix();
+    GLUquadric* quad=gluNewQuadric();
+    gluCylinder(quad,0.05,0.05,0.01,32,32);
+    gluDisk(quad,0.01,0.05,32,32);
+    glTranslatef(0,0.01,0);
+    gluDisk(quad,0.01,0.05,32,32);
+    glPopMatrix();
+}
+void draw_sidehold(){
+    /* Koeficijenti ambijentalne refleksije materijala. */
+    GLfloat ambient_coeffs[] = { 0.3, 0.3, 0.3 };
+
+    /* Koeficijenti difuzne refleksije materijala. */
+    GLfloat diffuse_coeffs[] = { 0.4, 0.4, 0.4};
+
+    /* Koeficijenti spekularne refleksije materijala. */
+    GLfloat specular_coeffs[] = { 0.775,0.775,0.775};
+
+    /* Koeficijent glatkosti materijala. */
+    GLfloat shininess = 20;
+
+
+    /* Podesavaju se parametri materijala. */
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient_coeffs);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse_coeffs);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular_coeffs);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
+
+    glShadeModel(GL_SMOOTH);
+    glPushMatrix();
+    glScalef(0.05,0.075,0.4);
+    glutSolidCube(1);
+    glPopMatrix();
+}
+void draw_chassey(){
+    /* Koeficijenti ambijentalne refleksije materijala. */
+    GLfloat ambient_coeffs[] = { 0.8, 0, 0, 1 };
+
+    /* Koeficijenti difuzne refleksije materijala. */
+    GLfloat diffuse_coeffs[] = { 0.8, 0, 0, 1 };
+
+    /* Koeficijenti spekularne refleksije materijala. */
+    GLfloat specular_coeffs[] = { 0.7,0.7,0, 1 };
+
+    /* Koeficijent glatkosti materijala. */
+    GLfloat shininess = 50;
+
+
+    /* Podesavaju se parametri materijala. */
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient_coeffs);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse_coeffs);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular_coeffs);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
+
+    glShadeModel(GL_SMOOTH);
+    glPushMatrix();
+    glScalef(0.2,0.05,0.3);
+    glutSolidCube(1);
+    glPopMatrix();
+}
+void draw_hold(){
+    /* Koeficijenti ambijentalne refleksije materijala. */
+    GLfloat ambient_coeffs[] = { 0.3, 0.3, 0.3 };
+
+    /* Koeficijenti difuzne refleksije materijala. */
+    GLfloat diffuse_coeffs[] = { 0.4, 0.4, 0.4};
+
+    /* Koeficijenti spekularne refleksije materijala. */
+    GLfloat specular_coeffs[] = { 0.775,0.775,0.775};
+
+    /* Koeficijent glatkosti materijala. */
+    GLfloat shininess = 20;
+
+
+    /* Podesavaju se parametri materijala. */
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient_coeffs);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse_coeffs);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular_coeffs);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
+
+    glShadeModel(GL_SMOOTH);
+    glPushMatrix();
+    glScalef(0.2,0.075,0.2);
+    glutSolidCube(1);
+    glPopMatrix();
+}
+void draw_guard_part(){
+    /* Koeficijenti ambijentalne refleksije materijala. */
+    GLfloat ambient_coeffs[] = { 0, 0.7, 0, 1 };
+
+    /* Koeficijenti difuzne refleksije materijala. */
+    GLfloat diffuse_coeffs[] = { 0, 0.7, 0, 1 };
+
+    /* Koeficijenti spekularne refleksije materijala. */
+    GLfloat specular_coeffs[] = { 0.7,0.7,0, 1 };
+
+    /* Koeficijent glatkosti materijala. */
+    GLfloat shininess = 80;
+
+
+    /* Podesavaju se parametri materijala. */
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient_coeffs);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse_coeffs);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular_coeffs);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
+
+    glShadeModel(GL_SMOOTH);
+    glPushMatrix();
+    glScalef(0.03,0.03,0.03);
+    glutSolidCube(1);
+    glPopMatrix();
+}
+void draw_hangar(){
+    /* Koeficijenti ambijentalne refleksije materijala. */
+    GLfloat ambient_coeffs[] = { 0.3, 0.3, 0.3 };
+
+    /* Koeficijenti difuzne refleksije materijala. */
+    GLfloat diffuse_coeffs[] = { 0.4, 0.4, 0.4};
+
+    /* Koeficijenti spekularne refleksije materijala. */
+    GLfloat specular_coeffs[] = { 0.775,0.775,0.775};
+
+    /* Koeficijent glatkosti materijala. */
+    GLfloat shininess = 20;
+
+
+    /* Podesavaju se parametri materijala. */
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient_coeffs);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse_coeffs);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular_coeffs);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
+
+    glShadeModel(GL_SMOOTH);
+    glPushMatrix();
+    glutSolidSphere(0.03,32,32);
+    glPopMatrix();
+}
+void draw_cannonball(){
+    /* Koeficijenti ambijentalne refleksije materijala. */
+    GLfloat ambient_coeffs[] = { 0.4, 0.4, 0.4 };
+
+    /* Koeficijenti difuzne refleksije materijala. */
+    GLfloat diffuse_coeffs[] = { 0.4, 0.4, 0.4};
+
+    /* Koeficijenti spekularne refleksije materijala. */
+    GLfloat specular_coeffs[] = { 0.775,0.775,0.775};
+
+    /* Koeficijent glatkosti materijala. */
+    GLfloat shininess = 20;
+
+
+    /* Podesavaju se parametri materijala. */
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient_coeffs);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse_coeffs);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular_coeffs);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
+
+    glShadeModel(GL_SMOOTH);
+    glPushMatrix();
+    glutSolidSphere(0.01,64,64);
+    glPopMatrix();
+}
+void draw_cannon(){
+    /* Koeficijenti ambijentalne refleksije materijala. */
+    GLfloat ambient_coeffs[] = { 0.3, 0.3, 0.3 };
+
+    /* Koeficijenti difuzne refleksije materijala. */
+    GLfloat diffuse_coeffs[] = { 0.4, 0.4, 0.4};
+
+    /* Koeficijenti spekularne refleksije materijala. */
+    GLfloat specular_coeffs[] = { 0.775,0.775,0.775};
+
+    /* Koeficijent glatkosti materijala. */
+    GLfloat shininess = 80;
+
+
+    /* Podesavaju se parametri materijala. */
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient_coeffs);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse_coeffs);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular_coeffs);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
+    glDisable(GL_CULL_FACE);
+    glShadeModel(GL_SMOOTH);
+    glPushMatrix();
+    GLUquadric* quad=gluNewQuadric();
+    gluCylinder(quad,0.01,0.01,0.75,32,32);
+    glPopMatrix();
+}
 static void on_display(void)
 {
     glMatrixMode(GL_MODELVIEW);
@@ -258,8 +519,99 @@ static void on_display(void)
     plot_plane();
     draw_cube();
     draw_sphere();
+    glPushMatrix();
+    glTranslatef(2.8,0.02,0);
+    draw_rails();
+    glTranslatef(0.01,0,0);
+    draw_rails();
+    glTranslatef(0.18,0,0);
+    draw_rails();
+    glTranslatef(0.01,0,0);
+    draw_rails();
+    glPopMatrix();
+    glPushMatrix();
+    glTranslatef(2.9,0,-10);
+    for(int i=0;i<=40;i++){
+	    draw_plank();
+	    glTranslatef(0,0,0.5);
+    }
+    glPopMatrix();
+    glPushMatrix();
+    glRotatef(90,0,0,1);
+    glTranslatef(0.05,-2.805,0);
+    glRotatef(90,1,0,0);
+    draw_wheel();
+    glTranslatef(0,0.25,0);
+    draw_wheel();
+    glTranslatef(0,0,0.19);
+    draw_wheel();
+    glTranslatef(0,-0.25,0);
+    draw_wheel();
+    glPopMatrix();
+    glPushMatrix();
+    glTranslatef(2.805,0.075,0.125);
+    draw_sidehold();
+    glTranslatef(0.195,0,0);
+    draw_sidehold();
+    glPopMatrix();
+    glPushMatrix();
+    glTranslatef(2.9,0.1,0.1);
+    draw_chassey();
+    glTranslatef(0,0.05,0);
+    draw_hold();
+    glTranslatef(0,0.05,0);
+    glPushMatrix();
+    for(int i=0;i<360;i+=20){
+        glPushMatrix();
+        glRotatef(i,0,1,0);
+        glTranslatef(0.05,0,0);
+        draw_guard_part();
+        glPopMatrix();
+    }
+    for(int i=0;i<3;i++){
+        glTranslatef(0,0.02,0);
+        for(int i=40;i<320;i+=20){
+            glPushMatrix();
+            glRotatef(i,0,1,0);
+            glTranslatef(0.05,0,0);
+            draw_guard_part();
+            glPopMatrix();
+        }
+    }
+    glPushMatrix();
+    glTranslatef(0,-0.05,0); 
+    glRotatef(90,0,1,0);
+    glRotatef(-45,1,0,0);
+    draw_hangar();
+    draw_cannon();
+    glPopMatrix();
+    glTranslatef(0,-0.05,0);
+    glTranslatef(0.75/sqrt(2),0.75/sqrt(2),0);
+    draw_cannonball();
+    glPopMatrix();
+    glPopMatrix();
     glPopMatrix();
     /* Nova slika se salje na ekran. */
     glutSwapBuffers();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
